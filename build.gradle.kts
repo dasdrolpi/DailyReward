ced5ad947c7dc5fd7b35f920e012ea326d4d6428
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-03-21 20:51:26 +0100

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,30 @@ plugins {
     id("maven-publish")
 }
 
-group = "de.drolpi"
-version = "1.0-SNAPSHOT"
+defaultTasks("build")
 
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
+allprojects {
+    group = "de.drolpi"
+    version = "1.0.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_17.toString()
+    targetCompatibility = JavaVersion.VERSION_17.toString()
+    // options
+    options.encoding = "UTF-8"
+    options.isIncremental = true
+}
+
+tasks.withType<Jar> {
+    archiveFileName.set("DailyReward.jar")
 }
