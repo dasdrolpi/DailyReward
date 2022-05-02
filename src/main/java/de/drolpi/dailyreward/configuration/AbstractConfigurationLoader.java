63505f85e95e9164a203e22d7ea2995d7a8bb2ae
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-05-02 18:59:37 +0200

    Code clean up

--- src/main/java/de/drolpi/dailyreward/configuration/AbstractConfigurationLoader.java
+++ src/main/java/de/drolpi/dailyreward/configuration/AbstractConfigurationLoader.java
@@ -9,7 +9,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.function.Consumer;
 
 public abstract class AbstractConfigurationLoader<T> {
 
--- src/main/java/de/drolpi/dailyreward/model/RewardPlayer.java
+++ src/main/java/de/drolpi/dailyreward/model/RewardPlayer.java
@@ -4,7 +4,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class RewardPlayer {
 
