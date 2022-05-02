5043f464da8ca031efd11f29212fc67bb8a4188d
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-05-02 18:54:08 +0200

    Improve RewardPlayer creation in RewardStorage#createPlayer

--- src/main/java/de/drolpi/dailyreward/storage/RewardStorage.java
+++ src/main/java/de/drolpi/dailyreward/storage/RewardStorage.java
@@ -2,6 +2,7 @@ package de.drolpi.dailyreward.storage;
 
 import de.drolpi.dailyreward.model.RewardPlayer;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,13 +16,15 @@ public class RewardStorage {
         this.playerContainer = new HashMap<>();
     }
 
-    public void createPlayer(Player player) {
-        this.playerContainer.put(player.getUniqueId(), new RewardPlayer());
+    public RewardPlayer createPlayer(Player player) {
+        var rewardPlayer = new RewardPlayer();
+        this.playerContainer.put(player.getUniqueId(), rewardPlayer);
+        return rewardPlayer;
     }
 
-    public RewardPlayer player(Player player) {
+    public @NotNull RewardPlayer player(Player player) {
         if (!this.playerContainer.containsKey(player.getUniqueId())) {
-            createPlayer(player);
+            return createPlayer(player);
         }
         return this.playerContainer.get(player.getUniqueId());
     }
