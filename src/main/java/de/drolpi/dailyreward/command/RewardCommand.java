3bf44f4d94addb8724daa9d51c259a7e9b76da13
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-05-02 18:49:38 +0200

    Add some NotNull annotations

--- src/main/java/de/drolpi/dailyreward/command/RewardCommand.java
+++ src/main/java/de/drolpi/dailyreward/command/RewardCommand.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class RewardCommand implements CommandExecutor {
 
@@ -16,7 +17,7 @@ public class RewardCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (!(sender instanceof Player player))
             return true;
 
